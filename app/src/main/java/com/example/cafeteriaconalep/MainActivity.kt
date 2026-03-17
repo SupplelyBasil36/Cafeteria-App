da625d23784a35abf2d69c617c9156687654b549
Luis Montoya <skyfire5990@gmail.com>
2026-03-17 10:09:23 -0600

    First version and push of app 2

--- app/src/main/java/com/example/cafeteriaconalep/MainActivity.kt
+++ app/src/main/java/com/example/cafeteriaconalep/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         rvListaPlatillos = findViewById(R.id.rvListaPlatillos)
         imprimirBoton = findViewById(R.id.btnImprimir)
         val adapter = PlatillosAdapter(PlatillosProvider.listaPlatillos)
