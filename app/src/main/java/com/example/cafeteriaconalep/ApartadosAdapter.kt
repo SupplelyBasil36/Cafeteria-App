9bb4d5ed5a13c1b048d7fd2cf384d5cb525cb2cc
Luis Acer <skyfire5990@gmail.com>
2026-03-23 21:31:49 -0600

    Se añade funcion para eliminar apartados

--- app/src/main/java/com/example/cafeteriaconalep/ApartadosAdapter.kt
+++ app/src/main/java/com/example/cafeteriaconalep/ApartadosAdapter.kt
@@ -3,8 +3,9 @@ package com.example.cafeteriaconalep
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class ApartadosAdapter(private val listaApartados: List<Apartados>) :
+class ApartadosAdapter(private val listaApartados: MutableList<Apartados>) :
     RecyclerView.Adapter<ApartadosViewHolder>() {
     override fun onCreateViewHolder(
         p0: ViewGroup,
@@ -19,13 +20,37 @@ class ApartadosAdapter(private val listaApartados: List<Apartados>) :
         p1: Int
     ) {
         val data = listaApartados[p1]
+        p0.apartadoCheck.setOnCheckedChangeListener(null)
 
         p0.nombreApartado.text = data.nombreApartado
         p0.descPlatillo.text = data.nombrePlatilloApartado
         p0.apartadoCheck.isChecked = data.seleccionado
 
         p0.apartadoCheck.setOnCheckedChangeListener { _, isChecked ->
-            data.seleccionado = isChecked
+            if (isChecked) {
+                MaterialAlertDialogBuilder(p0.itemView.context)
+                    .setTitle("Eliminar apartado")
+                    .setMessage("¿Estás seguro de que deseas eliminar \"${data.nombreApartado}\"?")
+                    .setNegativeButton("Cancelar") { dialog, _ ->
+                        p0.apartadoCheck.setOnCheckedChangeListener(null)
+                        p0.apartadoCheck.isChecked = false
+                        data.seleccionado = false
+                        p0.apartadoCheck.setOnCheckedChangeListener { _, isChecked ->
+                            data.seleccionado = isChecked
+                        }
+                        dialog.dismiss()
+                    }
+                    .setPositiveButton("Sí, eliminar") { _, _ ->
+                        val posicionActual = p0.adapterPosition
+                        if (posicionActual != RecyclerView.NO_ID.toInt()) {
+                            listaApartados.removeAt(posicionActual)
+                            notifyItemRemoved(posicionActual)
+                        }
+                    }
+                    .show()
+            } else {
+                data.seleccionado = false
+            }
         }
     }
 
