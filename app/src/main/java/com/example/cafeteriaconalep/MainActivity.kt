f82566ca279e73cb7df759b28cea019f99ddb2d1
Luis Montoya <skyfire5990@gmail.com>
2026-04-15 15:40:04 -0600

    Se añade menú lateral a la aplicacion

--- app/src/main/java/com/example/cafeteriaconalep/MainActivity.kt
+++ app/src/main/java/com/example/cafeteriaconalep/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -18,7 +19,6 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
     lateinit var rvListaPlatillos: RecyclerView
     lateinit var imprimirBoton: Button
-    lateinit var apartadosBoton: Button
     private val BLUETOOTH_PERMISSION_CODE = 100
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
@@ -31,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         //VISTAS------------------------------------
         rvListaPlatillos = findViewById(R.id.rvListaPlatillos)
         imprimirBoton = findViewById(R.id.btnImprimir)
-        apartadosBoton = findViewById(R.id.btnApartados)
 
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.navigationView)
@@ -66,7 +65,20 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.nav_corte -> {
-                    startActivity(Intent(this, CorteVentasActivity::class.java))
+                    val dialog = Dialog(this)
+                    dialog.setContentView(R.layout.activity_corte_ventas)
+                    dialog.show()
+                    val btnCorte = dialog.findViewById<Button>(R.id.btnGenerarCorte)
+
+                    btnCorte.setOnClickListener {
+                        val alertDialogCorte = AlertDialog.Builder(this)
+                        alertDialogCorte.setTitle("Generando corte")
+                        alertDialogCorte.setMessage("Se eliminará el total anterior, ¿Desea continuar?")
+                        alertDialogCorte.setPositiveButton("Aceptar") { dialog, _ ->
+                            dialog.cancel()
+                        }
+                        alertDialogCorte.show()
+                    }
                 }
             }
             drawerLayout.closeDrawer(GravityCompat.START)
@@ -127,28 +139,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }.start()
 
-
-            /*val builder = AlertDialog.Builder(this)
-            builder.setTitle("Imprimir")
-            builder.setMessage("La lista se esta imprimiendo")
-            builder.setPositiveButton("Aceptar") { dialog, which ->
-                // Acción al presionar Aceptar (puede estar vacía)
-                println(adapter.obtenerTotal()) //Obtenemos el costo total del ticket
-                adapter.limpiarSeleccionados()
-                dialog.cancel()
-            }
-            builder.setNegativeButton("Cancelar") { dialog, which ->
-                // Acción al presionar Cancelar
-            }
-            builder.show()*/
-            //Esto funciona para agregar un platillo a la lista
-            //PlatillosProvider.listaPlatillos.add(Platillos("Agregado extra", 55, false))
-            //adapter.notifyDataSetChanged()
-        }
-
-        apartadosBoton.setOnClickListener {
-            val intent = Intent(this, ApartadosActivity::class.java)
-            startActivity(intent)
         }
 
     }
